72c0f4c4135ffd0be18a23965021d34762e1c234
Bishnu Mishra <mishravishnu369@gmail.com>
2024-06-20 16:47:00 +0530

    repo setup 2

--- clear_class.bat
+++ clear_class.bat
@@ -2,13 +2,13 @@
 REM Batch file to delete all .exe files from the specified directory
 
 REM Define the path
-set targetPath=C:\Users\mishr\OneDrive\Desktop\Coding\OOPS\C++
+set targetPath=C:\Users\mishr\OneDrive\Desktop\Coding\OOPS\Java
 
 REM Change to the target directory
 cd /d "%targetPath%"
 
 REM Delete all .exe files
-del /s /q *.exe
+del /s /q *.class
 
 REM Inform the user that the operation is complete
 echo All .exe files have been deleted from %targetPath%
